577ddc0fb32e2f58cb63a9b1358cae67a2038d5e
jurados <jurado.steve.sj@gmail.com>
2022-02-24 22:55:36 -0500

    update diagram

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Hertzsprung-Rusell-Diagram
 
-This code generate a gen
+This code generate a [Hertzsprung-Rusell diagram](https://en.wikipedia.org/wiki/Hertzsprung%E2%80%93Russell_diagram) from a stellar astrophysics homework based in the text [An Introduction to Modern Astrophysics (2nd Edition) - Bradley W. Carroll & Dale A. Ostile](https://www.amazon.com/Introduction-Modern-Astrophysics-2nd/dp/0805304029).
+
+The homework's taks were:
+
+> The main sequence present an aproximate mass-luminosity relation as $L \propto M^{7/2}$ and a radius-mass relation as $R \propto M^{4/5}$.
+
+1. With the before relations you can graph the main sequence in an Hertzsprung-Rusell theoretical diagram (HRD). Your HRD should cover the range 3.5 - 4.5 for $\log{T_{eff}}$ and a appropiate range for $\log{L/L_{\odot}}$. Hint: Derivate first a expresion for $L$ in $T_{eff}$ terms for stars in main sequences.
+
+We now that the relation between the luminosity and the effective temperature for a star is:
+$$L = 4\pi R_{\star} \sigma T_{eff}^{4}$$
+if we divided with the Sun's values:
+$$\frac{L}{L_{\odot}} = \frac{4\pi R_{\star} \sigma T_{eff}^{4}}{4\pi R_{\odot} \sigma T_{eff,{\odot}}^{4}}$$
+
+
---
+++ hr-diagram.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 24 19:13:47 2022
+
+@author: steve
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
+
+star_data = pd.read_csv('./gappendix_mainsequence_carrol.csv',sep=',',
+                        header=0)
+TSUN = 5771 # Kelvin
+
+log_teff   = np.linspace(3.5,4.5,25)
+log_LLSUN = (35/19)*4*(log_teff-np.log10(TSUN))
+
+Rstar = (0.1,1,10,100) # R_SUN
+log_LR = lambda Rstar: 2*np.log10(Rstar)+4*(log_teff-np.log10(TSUN))
+
+fig, ax = plt.subplots()
+ax.plot(log_teff,log_LLSUN,color='black',markersize=4,alpha=0.7)
+
+for r in Rstar:
+    ax.plot(log_teff,log_LR(r),color='gray',
+            linestyle='dashed')
+    
+ax.text(log_teff[-2],log_LR(Rstar[0])[-8],'R = '+'{}'.format(Rstar[0])+'$R_{\odot}$')
+ax.text(log_teff[-2],log_LR(Rstar[1])[-8],'R = '+'{}'.format(Rstar[1])+'$R_{\odot}$')
+ax.text(log_teff[7],log_LR(Rstar[2])[8],'R = '+'{}'.format(Rstar[2])+'$R_{\odot}$')
+ax.text(log_teff[7],log_LR(Rstar[3])[8],'R = '+'{}'.format(Rstar[3])+'$R_{\odot}$')
+
+ax.scatter(np.log10(star_data['t_eff']),np.log10(star_data['lum_lsun']))
+ax.plot(np.log10(5771),0,'y*', label='Sun',markersize=10)
+ax.plot(np.log10(3500),np.log10(63000),'r*',markersize=10,label='Betelguese')
+ax.tick_params(axis='both',which='both',direction='in')
+ax.yaxis.set_minor_locator(tck.AutoMinorLocator())
+ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
+ax.legend(frameon=False,loc='lower left')
+ax.set_xlim(4.5,3.5) 
+ax.set_xlabel(r'$\log{T_{eff}}$')
+ax.set_ylabel(r'$\log{L/L_{\odot}}$')
